93781e99b5de33e7d1c04c6e5f6aef730238fed7
Bruno Lucero <brunolucero2419@gmail.com>
2024-06-14 16:52:45 -0300

    Se modificó el ejercicio 4.

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -6,12 +6,16 @@ class Rectangle{
         this.width = width
         this.height = height
     }
-
+    area(){
+        let resultado_area = this.width * this.height
+        console.log(`El ancho del rectángulo es: ${this.width} y el largo es: ${this.height} , por lo que el área del rectángulo es: ${resultado_area}`)
+    }
     perimetro(){
-        let resultado = (this.width*2) + (this.height*2)
-        console.log(`El ancho del rectángulo es: ${this.width} y el largo es: ${this.height} , por lo que el perímetro del rectángulo es: ${resultado}`)
+        let resultado_perimetro = (this.width*2) + (this.height*2)
+        console.log(`El ancho del rectángulo es: ${this.width} y el largo es: ${this.height} , por lo que el perímetro del rectángulo es: ${resultado_perimetro}`)
     }
 }
 
 let rectangulo1 = new Rectangle(10,20)
+rectangulo1.area()
 rectangulo1.perimetro()
